89c7c142db0373a3d4867a947449033b43d758dd
mkbeh <mkbehforever@gmail.com>
2020-06-24 02:46:14 +0300

    refactored

--- static/js/yandex.js
+++ static/js/yandex.js
@@ -1,9 +1,9 @@
 'use strict';
 
 
-// Preloader.
+// --- PRELOADER.
 function onReady(callback) {
-    var intervalId = window.setInterval(function() {
+    let intervalId = window.setInterval(function() {
       if (document.getElementsByTagName('body')[0] !== undefined) {
         window.clearInterval(intervalId);
         callback.call(this);
@@ -25,27 +25,51 @@ function setVisibleLevel(visible) {
 }
 
 
+function inputsPreparation() {
+    document.querySelectorAll('.input').forEach(function(element) {
+        element.value = '';
+    })
+
+    document.getElementById('input1').focus();
+}
+
+
 setVisibleLevel(false);
 
 
-
 onReady(function() {
+    inputsPreparation();
     runPreloader(true);
     runPreloader(false);
     setVisibleLevel(true);
 });
 
 
-
-// Appearing of hidden social buttons.
+// --- APPEREANCE OF HIDDEN SOCIAL BUTTONS.
 let moreBtn = document.getElementById('more');
-let socialIcons = document.getElementById('socialIcons');
 
 
 moreBtn.addEventListener('click', function() {
     moreBtn.style.display = 'none';
-
+    
+    let socialIcons = document.getElementById('socialIcons');
     for (var i = 4; i < socialIcons.children.length; i++) {
         socialIcons.children[i].style.display = 'block';
     }
 })
+
+
+
+// --- MOVING INPUT'S LABELS.
+document.querySelectorAll('.input').forEach(item => {
+    item.addEventListener('blur', event => {
+        let label = item.nextElementSibling;
+        
+    if (item.value) {
+        label.style.transform = 'translate(-23px, -1px) scale(.85)';
+    } else {
+        label.style.transform = '';
+        label.style.transform = 'translateY(25px); scale(1)';
+    }
+    })
+  })
